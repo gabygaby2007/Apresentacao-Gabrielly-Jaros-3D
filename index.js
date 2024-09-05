e1d87bd5a79a65196d9a0f936231e4043d3aa0ff
gabygaby2007 <127844758+gabygaby2007@users.noreply.github.com>
2024-09-05 01:18:23 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const primeiroNome = "Gabrielly";
+const segundoNome = "Jaros";
+const idade = 16;
+const altura = 1.59;
+const cidadeNatal = 'Curitiba';
+const peso = 50;
+
+console.log("meu primeiro nome é");
+console.log(primeiroNome);
+console.log("meu segundo nome é");
+console.log(segundoNome);
+console.log("minha idade é");
+console.log(idade);
+console.log("a minha altura é");
+console.log(altura);
+console.log("eu nasci em");
+console.log(cidadeNatal)
+console.log("eu peso");
+console.log(peso);
